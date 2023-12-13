eba2d93cd360a8690334c316f802659800ffee57
astrid <astrid.maria.grun@gmail.com>
2023-12-13 18:52:14 +0200

    initial commit

---
+++ login-page.js
@@ -0,0 +1,15 @@
+
+const logForm= document.getElementById("loginForm");
+const loginButton= document.getElementById("loginSubmit");
+
+loginButton.addEventListener("click",(e)=>{
+	const username=logForm.user.value;
+	const passw=logForm.pass.value;
+	if(username=="AdMiN0620" && passw=="KaZtLvCFaexY"){
+		document.cookie="browser_granted=1";
+		window.location.replace("file:///C:/WaS/admin%20page.html");
+		
+		}
+	else{ 
+		alert("ERROR 403 Access Denied");}
+})
